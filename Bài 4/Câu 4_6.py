d4c3bbcf5ea26e9b2bd943698db4565e0c717359
Dinhquyenan <aquyen015@gmail.com>
2024-11-27 10:31:34 +0700

    Create Câu 4_6.py

---
+++ Bài 4/Câu 4_6.py
@@ -0,0 +1,10 @@
+ht=input("Nhập họ và tên: ")
+# Tách họ và tên riêng
+parts = ht.split()
+ho = parts [0] 
+ten_dem = parts [1]
+ten = parts [2] 
+# In kết quả
+print("Họ:", ho)
+print("Tên đệm:", ten_dem)
+print("Tên riêng:",ten)
