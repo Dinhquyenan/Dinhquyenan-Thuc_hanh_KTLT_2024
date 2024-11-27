5a5245ae3f53602876e34f991eee7d37b9129048
Dinhquyenan <aquyen015@gmail.com>
2024-11-27 08:46:47 +0700

    Create Câu 2_2.py

---
+++ Bài 2/Câu 2_2.py
@@ -0,0 +1,11 @@
+import math;
+xl=int(input("Enter xl--->"))
+yl=int(input("Enter yl--->"))
+
+x2=int(input("Enter x2--->"))
+y2=int(input("Enter y2--->"))
+
+dl = (x2 - x1) * (x2 - x1);
+d2 = (y2 - yl) (y2 * - yl);
+res = math.sqrt(dl+d2)
+print("Khoang cach giua 2 diem:",res);
