54c9d648ce67540639bb707d8e33a9ecaf07e4c9
Dinhquyenan <aquyen015@gmail.com>
2024-11-27 10:09:49 +0700

    Create Câu 3_10.py

---
+++ Bài 3/Câu 3_10.py
@@ -0,0 +1,8 @@
+R=float(input("Nhap ban kinh r: "))
+import math;
+def Tinh (R):
+  Chu_vi = 2 * math.pi * R
+  print("Chu vi hinh tron: ",Chu_vi)
+  Dien_tich = math.pi * R**2
+  print("Dien tich hinh tron: ", Dien_tich) 
+Tinh (R)
