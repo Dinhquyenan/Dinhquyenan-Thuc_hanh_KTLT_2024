c530ebf1b75922107216356dd648c8c33aa87ad5
Dinhquyenan <aquyen015@gmail.com>
2024-11-27 10:44:03 +0700

    Create Câu 4_8.py

---
+++ Bài 4/Câu 4_8.py
@@ -0,0 +1,10 @@
+def tu_dai_nhat(s):
+  tuDaiNhat = ""
+  dsCacTu = s.split()
+  for tu in dsCacTu:
+    if (len(tu) > len (tuDaiNhat)) or (len (tu) == len (tuDaiNhat) and tu < tuDa 
+       tuDaiNhat = tu     
+  return tuDaiNhat
+s = input () 
+print(tudai nhat (s))
+
